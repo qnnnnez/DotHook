d629f74cb8052da08cf9d9f3eed733563a4c262e
qnnnnez <qnnnnez@live.com>
2017-03-30 23:08:40 +0800

    add CodeInjector and ReferenceResolver
    CodeInjector is used to inject classes and methods to a assembly
    and ReferenceResolver can replace all references to one type/method
    to another one.
    These two classes are still under development.

---
+++ DotHook/CodeInjector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace DotHook
+{
+    static class CodeInjector
+    {
+        static public void InjectMethod(TypeDefinition targetClass, MethodDefinition sourceMethod, string methodName=null)
+        {
+            if (methodName == null)
+                methodName = sourceMethod.Name;
+            var injectMethod = CloneMethod(sourceMethod);
+            targetClass.Methods.Add(injectMethod);
+            injectMethod.DeclaringType = targetClass;
+            FixReferences(injectMethod);
+        }
+
+        static public void HookMethod(MethodDefinition targetMethod, MethodDefinition hookMethod, ReferenceResolver resolver=null, string hookPrefix="__hooked__")
+        {
+            if(resolver == null)
+            {
+                resolver = new ReferenceResolver();
+                resolver.ScanAssembly(targetMethod.DeclaringType.Module.Assembly);
+            }
+
+            var injectMethod = CloneMethod(hookMethod);
+            // rename original method
+            injectMethod.Name = targetMethod.Name;
+            targetMethod.Name = hookPrefix + targetMethod.Name;
+            // add the new method
+            targetMethod.DeclaringType.Methods.Add(injectMethod);
+            // replace the original method with the new one
+            resolver.ReplaceAllReferences(targetMethod, hookMethod);
+            // process self-calls
+            for (int i = 0; i < injectMethod.Body.Instructions.Count; ++i)
+            {
+                var ins = injectMethod.Body.Instructions[i];
+                if (ins.OpCode == OpCodes.Call || ins.OpCode == OpCodes.Callvirt)
+                {
+                    if (ins.Operand == injectMethod)
+                    {
+                        ins.Operand = targetMethod;
+                    }
+                }
+            }
+        }
+
+        static public MethodDefinition CloneMethod(MethodDefinition source)
+        {
+            var newMethod = new MethodDefinition(source.Name, source.Attributes, source.ReturnType);
+            var il = newMethod.Body.GetILProcessor();
+            // copy all instructions
+            foreach(var sourceIns in source.Body.Instructions)
+            {
+                var newInstruction = il.Create(sourceIns.OpCode);
+                newInstruction.Operand = sourceIns.Operand;
+                il.Append(newInstruction);
+            }
+            // if a instruction whose operand is another instruction, we need to correct its operand
+            var newInstructions = newMethod.Body.Instructions;
+            foreach (var ins in newInstructions)
+            {
+                if (ins.Operand is Instruction)
+                {
+                    ins.Operand = newInstructions[(ins.Operand as Instruction).Offset];
+                }
+            }
+            return newMethod;
+        }
+        
+        static private void FixReferences(MethodDefinition method)
+        {
+            var module = method.Module;
+            for (int i=0; i<method.Body.Instructions.Count; ++i)
+            {
+                var ins = method.Body.Instructions[i];
+                if (ins.Operand is MethodReference)
+                {
+                    ins.Operand = module.ImportReference(ins.Operand as MethodReference);
+                }
+                else if (ins.Operand is TypeReference)
+                {
+                    ins.Operand = module.ImportReference(ins.Operand as TypeReference);
+                }
+                else if (ins.Operand is FieldReference)
+                {
+                    ins.Operand = module.ImportReference(ins.Operand as FieldReference);
+                }
+            }
+        }
+    }
+}
---
+++ DotHook/ReferenceSolver.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace DotHook
+{
+    class ReferenceResolver
+    {
+        public class InstructionResolveResult
+        {
+            public MethodDefinition method;
+            public Instruction instruction;
+            public InstructionResolveResult(MethodDefinition m, Instruction ins)
+            {
+                method = m;
+                instruction = ins;
+            }
+        }
+
+        public interface ITypeResolveResult { }
+
+        public class FieldTypeResolveResult : ITypeResolveResult
+        {
+            public FieldDefinition field;
+            public FieldTypeResolveResult(FieldDefinition f) { field = f; }
+        }
+        public class ReturnTypeTypeResolveResult : ITypeResolveResult
+        {
+            public MethodDefinition method;
+            public ReturnTypeTypeResolveResult(MethodDefinition m) { method = m; }
+        }
+        public class ParameterTypeResolveResult : ITypeResolveResult
+        {
+            public MethodDefinition method;
+            public ParameterDefinition parameter;
+            public ParameterTypeResolveResult(MethodDefinition m, ParameterDefinition p) { method = m; parameter = p; }
+        }
+        public class LocalVarTypeResolveResult : ITypeResolveResult
+        {
+            public VariableDefinition variable;
+            public LocalVarTypeResolveResult(VariableDefinition v) { variable = v; }
+        }
+        public class InstructionTypeResolveResult : ITypeResolveResult
+        {
+            public MethodDefinition method;
+            public Instruction instruction;
+            public InstructionTypeResolveResult(MethodDefinition m, Instruction ins) { method = m; instruction = ins; }
+        }
+
+
+        public List<AssemblyDefinition> AssemblyList { get; private set; }
+        public List<ModuleDefinition> ModuleList { get; private set; }
+        public List<TypeDefinition> TypeList { get; private set; }
+
+        public ReferenceResolver()
+        {
+            TypeList = new List<TypeDefinition>();
+            ModuleList = new List<ModuleDefinition>();
+            AssemblyList = new List<AssemblyDefinition>();
+        }
+
+        public void ScanAssembly(AssemblyDefinition assembly)
+        {
+            AssemblyList.Add(assembly);
+            foreach (var module in assembly.Modules)
+                ScanModule(module);
+        }
+
+        public void ScanModule(ModuleDefinition module)
+        {
+            ModuleList.Add(module);
+            foreach (var type in module.Types)
+                ScanType(type);
+        }
+
+        public void ScanType(TypeDefinition type)
+        {
+            TypeList.Add(type);
+        }
+
+        public List<InstructionResolveResult> FindAllReferences(MemberReference target)
+        {
+            return FindAllReferencesInInstructions(target);
+        }
+
+        public List<ITypeResolveResult> FindAllReferences(TypeReference target)
+        {
+            return FindAllReferencesEveryWhere(target);
+        }
+
+        public void ReplaceAllReferences(MemberReference origin, MemberReference alternative)
+        {
+            foreach (var reference in FindAllReferences(origin))
+            {
+                reference.instruction.Operand = alternative;
+            }
+        }
+
+        public void ReplaceAllReferences(TypeReference origin, TypeReference alternative)
+        {
+            foreach (var reference in FindAllReferences(origin))
+            {
+                if (reference is FieldTypeResolveResult)
+                {
+                    var r = reference as FieldTypeResolveResult;
+                    r.field.FieldType = alternative;
+                }
+                else if (reference is ReturnTypeTypeResolveResult)
+                {
+                    var r = reference as ReturnTypeTypeResolveResult;
+                    r.method.ReturnType = alternative;
+                }
+                else if (reference is ParameterTypeResolveResult)
+                {
+                    var r = reference as ParameterTypeResolveResult;
+                    r.parameter.ParameterType = alternative;
+                }
+                else if (reference is LocalVarTypeResolveResult)
+                {
+                    var r = reference as LocalVarTypeResolveResult;
+                    r.variable.VariableType = alternative;
+                }
+                else if (reference is InstructionTypeResolveResult)
+                {
+                    var r = reference as InstructionTypeResolveResult;
+                    r.instruction.Operand = alternative;
+                }
+                else
+                {
+                    throw new ArgumentException("Unknown resolve type.");
+                }
+            }
+        }
+
+        List<InstructionResolveResult> FindAllReferencesInInstructions(IMetadataTokenProvider target)
+        {
+            var result = new List<InstructionResolveResult>();
+            foreach (var type in TypeList)
+            {
+                foreach (var method in type.Methods)
+                {
+                    foreach (var ins in method.Body.Instructions)
+                    {
+                        if (ReferenceEquals(target, ins.Operand))
+                        {
+                            result.Add(new InstructionResolveResult(method, ins));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        List<ITypeResolveResult> FindAllReferencesEveryWhere(IMetadataTokenProvider target)
+        {
+            var result = new List<ITypeResolveResult>();
+            foreach (var type in TypeList)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (ReferenceEquals(target, method.ReturnType))
+                    {
+                        result.Add(new ReturnTypeTypeResolveResult(method));
+                    }
+
+                    foreach (var param in method.Parameters)
+                    {
+                        if (ReferenceEquals(target, param))
+                        {
+                            result.Add(new ParameterTypeResolveResult(method, param));
+                        }
+                    }
+
+                    foreach (var ins in method.Body.Instructions)
+                    {
+                        if (ReferenceEquals(target, ins.Operand))
+                        {
+                            result.Add(new InstructionTypeResolveResult(method, ins));
+                        }
+                    }
+
+                    foreach (var local in method.Body.Variables)
+                    {
+                        if (ReferenceEquals(target, local))
+                        {
+                            result.Add(new LocalVarTypeResolveResult(local));
+                        }
+                    }
+                }
+                
+                foreach(var field in type.Fields)
+                {
+                    if (ReferenceEquals(target, field.FieldType))
+                    {
+                        result.Add(new FieldTypeResolveResult(field));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
